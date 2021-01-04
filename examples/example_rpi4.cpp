744a962725e3f523afbd2440d9dea8be621d42b5
Batuhan Kından <batuhan.kindan@gmail.com>
2021-01-04 22:52:49 +0000

    Test I2C Interface - Wakeup Sensor

--- examples/example_rpi4.cpp
+++ examples/example_rpi4.cpp
@@ -7,9 +7,26 @@
   ******************************************************************************
   */
 #include <iostream>
+#include "../mpu6050.h"
+#include "bcm2835_i2c_if.h"
 
 int main() 
 {
-    std::cout << "Test output\n";
-    return EXIT_SUCCESS;
+  I2C_Interface* i2c_if = new BCM2835_I2C_IF();
+  /* initialize fast I2C 400 kHz */
+  if(i2c_if->Init_I2C(400000) != I2C_STATUS_SUCCESS)
+  {
+    std::cout << "I2C initialization failed!\n";
+    return EXIT_FAILURE;
+  }
+  MPU6050 sensor(i2c_if);
+  /* wakeup sensor from sleep mode */
+  if(sensor.WakeUpSensor() != I2C_STATUS_SUCCESS)
+  {
+    std::cout << "Sensor wakeup failed!\n";
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "Sensor awake!\n";
+  return EXIT_SUCCESS;
 }
