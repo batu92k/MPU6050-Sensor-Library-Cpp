a3c0f23c29ee1d8d9b6f31b9f2d745f7c92af525
Batuhan KINDAN <batuhan.kindan@gmail.com>
2022-03-06 22:28:54 +0100

    Copy the Basic Reading Example as Bade of Fifo Example
    related with #3

---
+++ examples/ESP32/sensor_fifo_read/main/esp32_i2c_if.h
@@ -0,0 +1,58 @@
+/**
+  ******************************************************************************
+  * @file    esp32_i2c_if.h
+  * @author  Ali Batuhan KINDAN
+  * @date    24.01.2022
+  * @brief   This file constains the imlementation I2C interface for esp32-wroom microcontroller.
+  ******************************************************************************
+  */
+
+#include "i2c_interface.h"
+
+#ifndef ESP32_I2C_IF_H
+#define ESP32_I2C_IF_H
+
+/* I2C interface for ESP32-Wrrom MCU. */
+class ESP32_I2C_IF : public I2C_Interface
+{
+public:
+  /**
+  * @brief  I2C peripheral initialization method.
+  * @param  clock I2C clock frequency (default 100 kHz)
+  * @retval i2c_status_t
+  */
+  i2c_status_t Init_I2C(i2c_clockspeed_t clock = i2c_clockspeed_t::CLK_100KHz) override;
+
+  /**
+  * @brief  This method will be used for reading the data of the given register from
+  * the slave with given address.
+  * @param  slaveAddress Slave chip I2C bus address
+  * @param  regAddress Register address to be read
+  * @param  status Pointer for operation status
+  * @retval uint8_t Read register value
+  */
+  uint8_t ReadRegister(uint8_t slaveAddress, uint8_t regAddress, i2c_status_t *status) override;
+
+  /**
+  * @brief  This method will be used for writing gven data to the given register of the slave device 
+  * with the given address.
+  * @param  slaveAddress Slave chip I2C bus address
+  * @param  regAddress Register address that the data to be written
+  * @param  data Data to be written
+  * @retval i2c_status_t
+  */
+  i2c_status_t WriteRegister(uint8_t slaveAddress, uint8_t regAddress, uint8_t data) override;
+
+  /**
+  * @brief  Class destructor.
+  * @param  none
+  * @retval none
+  */  
+  ~ESP32_I2C_IF() override;
+  private:
+  const uint8_t I2C_Master_Port = 0;
+  const uint16_t I2C_Timeout_ms = 100;
+
+};
+
+#endif
---
+++ examples/ESP32/sensor_fifo_read/main/fifo_read_esp32.cpp
@@ -0,0 +1,122 @@
+/**
+  ******************************************************************************
+  * @file    fifo_read_esp32.cpp
+  * @author  Ali Batuhan KINDAN
+  * @date    06.03.2022
+  * @brief   Example application for ESP32-Wroom to show MPU6050 fifo reading sequence.
+  *          IDF version used: ESP-IDF v5.0-dev-1295-gfaf0f61cdb
+  ******************************************************************************
+  */
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+#include "../../../../mpu6050.h"
+#include "esp32_i2c_if.h"
+
+extern "C" void app_main(void)
+{
+  /* Print chip information */
+  esp_chip_info_t chip_info;
+  esp_chip_info(&chip_info);
+  printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
+         CONFIG_IDF_TARGET,
+         chip_info.cores,
+         (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+         (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+  printf("silicon revision %d, ", chip_info.revision);
+  printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+         (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+  printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+
+  I2C_Interface *i2c_if = new ESP32_I2C_IF();
+  /* initialize fast I2C 400 kHz */
+  if (i2c_if->Init_I2C(i2c_clockspeed_t::CLK_400KHz) != I2C_STATUS_SUCCESS) {
+    printf("I2C initialization failed!\n");
+    delete i2c_if;
+    esp_restart();
+  }
+
+  MPU6050_Driver::MPU6050 sensor(i2c_if);
+  if(sensor.ResetSensor() != I2C_STATUS_SUCCESS)
+  {
+    printf("Sensor reset failed!\n");
+    esp_restart();
+  }
+  /* Simple safety delay after sensor reset */
+  vTaskDelay(100 / portTICK_RATE_MS);
+  
+  /* Wakeup sensor and set full scale ranges */
+  if(sensor.InitializeSensor(MPU6050_Driver::Gyro_FS_t::FS_1000_DPS, MPU6050_Driver::Accel_FS_t::FS_8G) != I2C_STATUS_SUCCESS)
+  {
+    printf("Sensor initialization failed!\n");
+    esp_restart();
+  }
+
+  /* Auto-Calibrate gyroscope registers to target value 0 (default) */
+  if(sensor.Calibrate_Gyro_Registers() != I2C_STATUS_SUCCESS)
+  {
+    printf("Gyro calibration failed!\n");
+    esp_restart();
+  }
+
+  /* Auto-Calibrate accelerometer registers to target values by default: 
+   * X = 0 MG, Y = 0 MG, Z = 1 MG */
+  if(sensor.Calibrate_Accel_Registers() != I2C_STATUS_SUCCESS)
+  {
+    printf("Accel calibration failed!\n");
+    esp_restart();
+  }
+
+  /* set digital low pass to default value 
+   * (just to show the feature it already has default value in startup) */
+  if(sensor.SetSensor_DLPF_Config(MPU6050_Driver::DLPF_t::BW_260Hz) != I2C_STATUS_SUCCESS) {
+    printf("DLPF configuration failed!\n");
+    esp_restart();  
+  }
+
+  /* set sample rate divider to default value 
+   * (just to show the feature it already has default value in startup) */
+  i2c_status_t error = I2C_STATUS_NONE;
+  if(sensor.SetGyro_SampleRateDivider(0) != I2C_STATUS_SUCCESS) {
+    printf("Sample rate config failed!\n");
+    esp_restart();  
+  }
+
+  float currentSampleRateHz = sensor.GetSensor_CurrentSampleRate_Hz(&error);
+  if(error != I2C_STATUS_SUCCESS) {
+    printf("Sample rate reading failed!\n");
+    esp_restart();  
+  }
+
+  printf("Sensor sample rate: %.2f Hz \n", currentSampleRateHz);
+  printf("Sensor configuration completed!\n");
+
+  /* Read sensor conversion registers for test! */
+  uint8_t totalRes = I2C_STATUS_SUCCESS;
+  i2c_status_t currentRes = I2C_STATUS_SUCCESS;
+  while(totalRes == I2C_STATUS_SUCCESS)
+  {
+    printf("Acc_X: %d ", sensor.GetAccel_X_Raw(&currentRes));
+    totalRes |= currentRes;
+    printf("Acc_Y: %d ", sensor.GetAccel_Y_Raw(&currentRes));
+    totalRes |= currentRes;
+    printf("Acc_Z: %d ", sensor.GetAccel_Z_Raw(&currentRes));
+    totalRes |= currentRes;
+    printf("Temp: %.2f ", sensor.GetTemperature_Celcius(&currentRes));
+    totalRes |= currentRes;
+    printf("Gyro_X: %d ", sensor.GetGyro_X_Raw(&currentRes));
+    totalRes |= currentRes;
+    printf("Gyro_Y: %d ", sensor.GetGyro_Y_Raw(&currentRes));
+    totalRes |= currentRes;
+    printf("Gyro_Z: %d \n", sensor.GetGyro_Z_Raw(&currentRes));
+    totalRes |= currentRes;
+    vTaskDelay(50 / portTICK_RATE_MS);
+  }
+
+  printf("Sensor read error! Program terminated!\n");
+  esp_restart();  
+}
